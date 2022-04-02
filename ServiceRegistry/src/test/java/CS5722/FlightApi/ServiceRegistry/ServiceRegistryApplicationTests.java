fff4aaac8f38c641419a6c63f17f539b7bbe2ca1
prasad.nijsure <prasad.nijsure3@gmail.com>
2022-04-02 18:22:26 +0100

    ServiceRegistry

---
+++ ServiceRegistry/src/test/java/CS5722/FlightApi/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package CS5722.FlightApi.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
