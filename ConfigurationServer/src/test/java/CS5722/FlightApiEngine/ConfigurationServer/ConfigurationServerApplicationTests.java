94b48eba4343416e233c5a9a204564bc39eafbc1
prasad.nijsure <prasad.nijsure3@gmail.com>
2022-04-02 18:20:24 +0100

    ConfigurationServer

---
+++ ConfigurationServer/src/test/java/CS5722/FlightApiEngine/ConfigurationServer/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package CS5722.FlightApiEngine.ConfigurationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
